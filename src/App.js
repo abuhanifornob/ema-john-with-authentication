6a9ca137fce051df7640cbbb7811589aa6e1af3d
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-10-18 18:36:58 +0600

    Login and Sinup route add

--- src/App.js
+++ src/App.js
@@ -6,41 +6,47 @@ import Shop from './components/Shop/Shop';
 import Orders from './components/Orders/Orders';
 import Inventory from './components/Inventory/Inventory';
 import { productsAndCartLoader } from './loaders/productsAndCartLoader';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path:'/',
-      element: <Main></Main>,
-      children: [
-        {
-          path: '/',
-          loader: () => fetch('products.json'),
-          element: <Shop></Shop>
+    const router = createBrowserRouter([{
+            path: '/',
+            element: < Main > </Main>,
+            children: [{
+                    path: '/',
+                    loader: () => fetch('products.json'),
+                    element: < Shop > </Shop>
+                },
+                {
+                    path: 'orders',
+                    loader: productsAndCartLoader,
+                    element: < Orders > </Orders>
+                },
+                {
+                    path: 'inventory',
+                    element: < Inventory > </Inventory>
+                },
+                {
+                    path: 'about',
+                    element: < About > </About>
+                },
+                {
+                    path: "login",
+                    element: < Login > </Login>
+                },
+                {
+                    path: "signup",
+                    element: <Signup> </Signup>
+                }
+            ]
         },
-        {
-          path:'orders',
-          loader: productsAndCartLoader,
-          element: <Orders></Orders>
-        },
-        {
-          path: 'inventory',
-          element: <Inventory></Inventory>
-        },
-        {
-          path:'about',
-          element:<About></About>
-        }
-      ]
-    },
-    
-  ])
-  return (
-    <div>
-      <RouterProvider router={router}></RouterProvider>
-    </div>
-  );
+
+    ])
+    return ( <div>
+        <RouterProvider router = { router } > </RouterProvider> </div >
+    );
 }
 
 export default App;
---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA0QfXl9HoKUVS3Zr8zlNKG7PL6vjy03Bk",
+    authDomain: "ema-john-with-authentica-e21af.firebaseapp.com",
+    projectId: "ema-john-with-authentica-e21af",
+    storageBucket: "ema-john-with-authentica-e21af.appspot.com",
+    messagingSenderId: "467857976109",
+    appId: "1:467857976109:web:82dae9e2a23e57b73441a4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;

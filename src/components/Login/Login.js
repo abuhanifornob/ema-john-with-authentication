f825fa221815405abe1ff882f8be1a8daa295aef
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-10-18 20:04:13 +0600

    Login Form Designe

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h1>This is Logine Page</h1>
+        <div className='login-container'>
+            <h1 className='login-logo'>Login</h1>
+            <form action="">
+                <div className='form-control'>
+                    <label htmlFor="">Email</label>
+                    <input type="email" name="email" required />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor="">Password</label>
+                    <input type="password" name="password" required />
+                </div>
+               <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <div className='someText'>
+            <p>New to Ema-jon?<Link to="/signup">Create New Account</Link></p>
+            </div>
+            
         </div>
     );
 };
